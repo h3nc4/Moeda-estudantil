ce11a9fb9d279f328663b2ccf781782f8f540065
h3nc4 <me@h3nc4.com>
2023-11-09 16:14:13 -0300

    dev: início dos decoradores

---
+++ src/logic/permissions.py
@@ -0,0 +1,58 @@
+#  Copyright 2023 Henrique Almeida, Gabriel Dolabela, João Pauletti
+# 
+# This file is part of Moeda estudantil.
+# 
+# Moeda estudantil is free software: you can redistribute it and/or modify it
+# under the terms of the GNU Affero General Public License as published by the Free
+# Software Foundation, either version 3 of the License, or (at your option) any
+# later version.
+# 
+# Moeda estudantil is distributed in the hope that it will be
+# useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero
+# General Public License for more details.
+# 
+# You should have received a copy of the GNU Affero
+# General Public License along with Moeda estudantil. If not, see
+# <https://www.gnu.org/licenses/>.
+
+from django.shortcuts import render
+
+# Página de erro 403, 'forbidden'
+def err403(request):
+    return render(request, '403.html', status=403)
+
+def somente_super(view_func):
+    def wrapper(request, *args, **kwargs):
+        if not request.user.is_superuser:
+            return err403(request)
+        return view_func(request, *args, **kwargs)
+    return wrapper
+
+def somente_empresa(view_func):
+    def wrapper(request, *args, **kwargs):
+        if not request.user.empresa:
+            return err403(request)
+        return view_func(request, *args, **kwargs)
+    return wrapper
+
+def somente_aluno(view_func):
+    def wrapper(request, *args, **kwargs):
+        if not request.user.aluno:
+            return err403(request)
+        return view_func(request, *args, **kwargs)
+    return wrapper
+
+def somente_professor(view_func):
+    def wrapper(request, *args, **kwargs):
+        if not request.user.professor:
+            return err403(request)
+        return view_func(request, *args, **kwargs)
+    return wrapper
+
+def ou_professor_ou_aluno(view_func):
+    def wrapper(request, *args, **kwargs):
+        if not (request.user.aluno or request.user.professor):
+            return err403(request)
+        return view_func(request, *args, **kwargs)
+    return wrapper
