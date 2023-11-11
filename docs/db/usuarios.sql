ce727acadd82aa1923133cdfc205c856023ec763
JPPauletti <jpedropauletti@gmail.com>
2023-11-11 12:05:35 -0300

    dev: SQL scripts

--- docs/db/usuarios.sql
+++ docs/db/usuarios.sql
@@ -1,22 +1,22 @@
 CREATE OR REPLACE VIEW public.usuarios
  AS
- SELECT user.id AS user_id,
-    user.last_login,
-    user.is_superuser,
-    user.username,
-    user.is_active,
-    user.date_joined,
-    user.email,
-    user.moedas,
+ SELECT u.id AS user_id,
+    u.last_login,
+    u.username,
+    u.is_active,
+    u.date_joined,
+    u.email,
+    u.moedas,
     COALESCE(aluno.cpf, professor.cpf) AS cpf,
     aluno.rg,
+    u.is_superuser AS is_admin,
     aluno.id AS aluno_id,
     empresa.id AS empresa_id,
     professor.id AS professor_id
-   FROM logic_usuario user
-     LEFT JOIN logic_aluno aluno ON user.aluno_id = aluno.id
-     LEFT JOIN logic_empresa empresa ON user.empresa_id = empresa.id
-     LEFT JOIN logic_professor professor ON user.professor_id = professor.id;
+   FROM logic_usuario u
+     LEFT JOIN logic_aluno aluno ON u.aluno_id = aluno.id
+     LEFT JOIN logic_empresa empresa ON u.empresa_id = empresa.id
+     LEFT JOIN logic_professor professor ON u.professor_id = professor.id;
 
 ALTER TABLE public.usuarios
     OWNER TO developer;
